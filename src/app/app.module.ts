d0bed0b002b00316524280b053d1cb3d4874b24d
Tavi <taviatwork@gmail.com>
2020-11-09 11:43:59 +0200

    Added create event page and links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ToastrService } from './common/toastr.service';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
+import { CreateEventComponent } from './events/create-event.component';
 @NgModule({
   imports: [
     BrowserModule,
@@ -20,6 +21,7 @@ import { appRoutes } from './routes';
     EventsListComponent,
     EventThumbnailComponent,
     EventDetailsComponent,
+    CreateEventComponent,
     NavBarComponent
   ],
   providers: [EventService, ToastrService],
