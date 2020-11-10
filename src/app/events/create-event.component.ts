a949dba0155ec99931803cf82f7bb78f73f6a7a8
Tavi <taviatwork@gmail.com>
2020-11-10 15:49:13 +0200

    Add new event

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -1,24 +1,27 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { EventService } from './shared';
 
 @Component({
-  template: `
-  <h1>New Event</h1>
-  <hr>
-  <div class="col-md-6">
-    <h3>[Create event for here]</h3>
-    <br>
-    <br>
-    <button type="submit" class="btn btn-primary">Save</button>
-    <button type="button" class="btn btn-default"
-    (click)="cancel()">Cancel</button>
-  </div>
-  `
+  templateUrl: './create-event.component.html',
+  styles: [`
+    em {floar:right; color:#E05C65; padding-left: 10px;}
+    .error input {background-color:#E3C3C5;}
+    .error ::-webkit-input-placeholder {color: #999}
+`]
 })
 export class CreateEventComponent {
   isDirty = true;
-  constructor(private router: Router) {
+  newEvent: any;
+  constructor(private router: Router,
+    private eventService: EventService
+    ) {
+  }
 
+  saveEvent(formValues) {
+    this.eventService.saveEvent(formValues);
+    this.isDirty = false;
+    this.router.navigate(['/events']);
   }
 
   cancel() {
