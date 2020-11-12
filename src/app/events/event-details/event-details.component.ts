90bc0c75ab87934a9f60b08602cda4dee18f8d6a
Tavi <taviatwork@gmail.com>
2020-11-12 11:16:03 +0200

    Adding filtering data

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -16,6 +16,7 @@ import { IEvent, ISession } from '../shared/index';
 export class EventDetailsComponent implements OnInit {
   event: IEvent;
   addMode: boolean;
+  filterBy = 'all';
 
   constructor(private eventService: EventService,
     private route: ActivatedRoute) { }
