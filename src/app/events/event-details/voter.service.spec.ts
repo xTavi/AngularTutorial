a6a5b56741a321a187d8110200a12c4f15a3f3ec
Tavi <taviatwork@gmail.com>
2020-11-17 18:42:01 +0200

    Added tests for voter service

---
+++ src/app/events/event-details/voter.service.spec.ts
@@ -0,0 +1,48 @@
+import { Mock } from 'protractor/built/driverProviders';
+import { of } from 'rxjs';
+import { ISession } from '../shared';
+import { VoterService } from './voter.service';
+
+describe('VoterService', () => {
+  let voterService: VoterService,
+  mockHttp;
+
+  beforeEach(() => {
+    mockHttp = jasmine.createSpyObj('mockHttp', ['delete', 'post']);
+    voterService = new VoterService(mockHttp);
+  });
+
+  describe('Delete Voter', () => {
+
+    it('should remove the voter from the list of voters', () => {
+      const session = { id: 6, voters: ['joe', 'john']};
+      mockHttp.delete.and.returnValue(of(false));
+
+      voterService.deleteVoter(3, <ISession>session, 'joe');
+
+      expect(session.voters.length).toBe(1);
+      expect(session.voters[0]).toBe('john');
+    });
+
+    it('should call http.delete with the right URL', () => {
+      const session = { id: 6, voters: ['joe', 'john']};
+      mockHttp.delete.and.returnValue(of(false));
+
+      voterService.deleteVoter(3, <ISession>session, 'joe');
+
+      expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe');
+    });
+  });
+
+  describe('addVoter', () => {
+
+    it('should call http.post with the right URL', () => {
+      const session = { id: 6, voters: ['john']};
+      mockHttp.post.and.returnValue(of(false));
+
+      voterService.addVoter(3, <ISession>session, 'joe');
+
+      expect(mockHttp.post).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe', {}, jasmine.any(Object));
+    });
+  });
+});
